6f60e9c9599ce4167aa1373ccaf10b03e4d33b14
hedgehog13 <ira.varshavsky@gmail.com>
2025-02-24 18:20:42 -0500

    Create FileUploadView.vue and set up the routing

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,17 @@
-import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "@/views/HomeView.vue";
+import { createRouter, createWebHistory } from 'vue-router';
+import FileUploadView from '@/views/FileUploadView.vue';
 
 const routes = [
-  { path: "/", name: "Home", component: HomeView },
+  {
+    path: '/',
+    name: 'Home',
+    component: FileUploadView,
+  },
+  // You can add other routes here as you expand the app
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes,
 });
 
---
+++ src/views/FileUploadView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2>Upload a File to Scan</h2>
+    <FileUpload />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import FileUpload from '@/components/FileUpload.vue'
+
+export default defineComponent({
+  name: 'FileUploadView',
+  components: {
+    FileUpload,
+  },
+})
+</script>
+
+<style scoped>
+/* Add any styles specific to this view if needed */
+</style>
