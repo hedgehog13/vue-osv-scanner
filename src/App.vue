989687466defe53b4122ad5864067021aff82d33
hedgehog13 <ira.varshavsky@gmail.com>
2025-02-24 17:59:01 -0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <router-view />
+</template>
+
+<script lang="ts" setup></script>
+
+<style scoped>
+/* Global styles can go here */
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>OSV Scanner</h1>
+    <p>Upload a package.json file to check for vulnerabilities.</p>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style scoped>
+h1 {
+  color: #007bff;
+}
+</style>
